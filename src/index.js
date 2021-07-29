2c611baf1b143f6ec0ff0ab4d23d844afd52105e
alexpataman[mac] <alex.pataman@gmail.com>
2021-07-30 00:08:01 +0200

    refactor: code refactoring

--- src/index.js
+++ src/index.js
@@ -4,71 +4,74 @@ function eval() {
 }
 
 const expressionCalculator = (str) => {
-    // remove all spaces
-    str = str.replace(/\s/g, '');
 
-    // stack for checking brackets
-    let stack = [];
-
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === ')') {
-            if (!stack.length) {
-                throw 'ExpressionError: Brackets must be paired';
+    // order of operators is important
+    const operations = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '/': (a, b) => {
+            if (b === 0) {
+                throw 'TypeError: Division by zero.';
             }
-
-            let toCalculate = str.slice(stack[stack.length - 1] + 1, i);
-            let calculated = doCalculate(toCalculate);
-
-            str = str.slice(0, stack[stack.length - 1]) + calculated + str.slice(i + 1);
-            i = stack.pop();
-        } else if (str[i] === '(') {
-            stack.push(i);
+            return (a / b);
         }
     }
-    if (stack.length) {
-        throw 'ExpressionError: Brackets must be paired';
+    const operators = Object.keys(operations);
+
+    const processExpression = (expr) => {
+        for (let operator of operators) {
+            // don't match beginning of the string and don't match one by one operators
+            let regexPattern = `(?!^)(?<!\\${operators.map(el => '\\' + el).join('|')})\\${operator}`;
+            let found = [...expr.matchAll(new RegExp(regexPattern, 'g'))];
+            let matchedOperation = found[found.length - 1];
+
+            if (matchedOperation) {
+                let x = expr.slice(0, matchedOperation.index);
+                let y = expr.slice(matchedOperation.index + 1);
+
+                return y ? operations[matchedOperation[0]](processExpression(x), processExpression(y)) : x;
+            }
+        }
+
+        return +expr;
     }
 
-    return doCalculate(str);
-}
+    const calculate = (str) => {
+        // remove all spaces
+        str = str.replace(/\s/g, '');
 
-function doCalculate(expr) {
-    const operators = ['+', '-', '*', '/'];
+        // stack for checking brackets
+        let stack = [];
 
-    const calculate = (operation, a, b) => {
-        a = +a, b = +b;
-
-        switch (operation) {
-            case '+':
-                return a + b;
-            case '-':
-                return a - b;
-            case '*':
-                return a * b;
-            case '/':
-                if (b === 0) {
-                    throw 'TypeError: Division by zero.';
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] === ')') {
+                if (!stack.length) {
+                    throw 'ExpressionError: Brackets must be paired';
                 }
-                return a / b;
+
+                // expression in brackets, should be calculated
+                let toCalculate = str.slice(stack[stack.length - 1] + 1, i);
+
+                let calculated = processExpression(toCalculate);
+
+                // combine the whole expression again
+                str = str.slice(0, stack[stack.length - 1]) + calculated + str.slice(i + 1);
+
+                // return index back to the latest position of open bracket
+                i = stack.pop();
+
+            } else if (str[i] === '(') {
+                stack.push(i);
+            }
         }
+        if (stack.length) {
+            throw 'ExpressionError: Brackets must be paired';
+        }
+        return processExpression(str);
     }
 
-    for (let operator of operators) {
-        //let regexPattern = `(?!^)(?<!\\/|\\*|\\+|\\-)\\${operator}`;
-        // don't match beginning of the string and don't match one by one operators
-        let regexPattern = `(?!^)(?<!\\${operators.map(el => '\\' + el).join('|')})\\${operator}`;
-        let found = [...expr.matchAll(new RegExp(regexPattern, 'g'))];
-        let matchedOperation = found[found.length - 1];
-
-        if (matchedOperation) {
-            let x = expr.slice(0, matchedOperation.index);
-            let y = expr.slice(matchedOperation.index + 1);
-
-            return y ? calculate(matchedOperation[0], doCalculate(x), doCalculate(y)) : x;
-        }
-    }
-
-    return +expr;
+    return calculate(str);
 }
 
 module.exports = {
